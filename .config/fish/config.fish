749dcc0fefd760b4ede3ca29311ba3fac02a9aba
Anonymous <Anonymous>
2022-05-16 17:43:28 +0200

    Update fish environment variables, switch to alacritty and update sway config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
 cat ~/.cache/wal/sequences
+set -x XDG_DATA_DIRS "$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
+set -x XDG_SESSION_TYPE wayland
+set -x XDG_CURRENT_DESKTOP sway
+set -x MOZ_ENABLE_WAYLAND 1
+set -x QT_QPA_PLATFORM wayland
